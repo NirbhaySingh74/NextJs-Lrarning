e690c54c7cde033a3adb94dfd1e472c6fe48a06f
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2024-09-21 20:07:44 +0530

    added signup and signin page

---
+++ app/signin/page.tsx
@@ -0,0 +1,8 @@
+const signin = () => {
+  return (
+    <div>
+      <h1>signin page</h1>
+    </div>
+  );
+};
+export default signin;
---
+++ app/signup/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div>
+      <p>This is Signup page</p>
+    </div>
+  );
+};
+
+export default page;
