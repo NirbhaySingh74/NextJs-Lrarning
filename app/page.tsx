b00045ebf544336feb7b5341e2a6cda476f16a50
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2024-09-23 16:59:32 +0530

    fetched data in Next.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
-export default function Home() {
+import axios from "axios";
+
+async function getUserDetails() {
+  const response = await axios.get(
+    "https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details"
+  );
+  return response.data;
+}
+
+export default async function Home() {
+  const userData = await getUserDetails();
+
   return (
-    <div>
-      <h1>hii Nirbhay</h1>
+    <div className="flex flex-col justify-center h-screen">
+      <div className="flex justify-center">
+        <div className="border p-8 rounded">
+          <div>Name: {userData?.name}</div>
+
+          {userData?.email}
+        </div>
+      </div>
     </div>
   );
 }
