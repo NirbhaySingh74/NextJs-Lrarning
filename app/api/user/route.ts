d821eb6fe9925e1c126aaaa79e1c46059d010797
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2024-09-24 19:22:56 +0530

    signup data added in database

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest } from "next/server";
-
+import { PrismaClient } from "@prisma/client";
+const client = new PrismaClient();
 export function GET() {
   return Response.json({
     email: "nirbhay@gmail.com",
@@ -11,7 +12,12 @@ export async function POST(req: NextRequest) {
   // extract the body
   const body = await req.json();
   // store the body in the database
-  console.log(body);
+  await client.user.create({
+    data: {
+      username: body.username,
+      password: body.password,
+    },
+  });
 
   return Response.json({
     message: "You are logged in!",
