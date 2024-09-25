d18b1921aeec78324578201f24f27be9ec2b8507
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2024-09-25 22:47:17 +0530

    redirected user to signup page after signup

--- components/Signup.tsx
+++ components/Signup.tsx
@@ -41,7 +41,7 @@ export function SignupComponent() {
                     username,
                     password,
                   });
-                  // router.push("/");
+                  router.push("/");
                 }}
                 type="button"
                 className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
